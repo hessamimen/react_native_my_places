23c81bd31fd941a716207295a1cdb8a6b5498830
Hessam <hessam.imen@gmail.com>
2023-11-14 20:24:44 +1100

    feat: implement location picker component

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -71,6 +71,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: Colors.primary100,
+    borderRadius: 4,
   },
   image: {
     width: "100%",
